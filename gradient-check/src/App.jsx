c8a5c9a25360203fc06ca1a7491bc27ea10d996d
Om Gupta <your_email@example.com>
2026-01-05 21:09:53 +0530

    last stage

---
+++ gradient-check/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   
+<div className="h-14 bg-linear-to-r from-cyan-500 to-red-500">hi,Om Gupta This Side</div>
+  )
+}
+
+export default App
